4ddbdc626d769001fff06427caa8e8a605051d5f
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-02-05 16:48:01 +0200

    Change font to noto sans

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import SiteNavigation from './components/layout/SiteNavigation.vue'
 </script>
 
 <template>
-  <div class="relative flex flex-col items-center max-7xl">
+  <div class="relative flex flex-col items-center max-7xl min-h-[100vh]">
     <SiteNavigation />
 
     <RouterView />
