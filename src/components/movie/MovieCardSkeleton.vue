e4f6068822c4cee1f6b036b1658044cf7081553d
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-02-05 17:13:53 +0200

    Create movie card skeleton component

---
+++ src/components/movie/MovieCardSkeleton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AnimatedPlaceholder from '../ui/AnimatedPlaceholder.vue'
+</script>
+<template>
+  <div
+    class="relative h-full w-full min-h-[595px] sm:min-h-[40vh] mx-auto overflow-hidden transition duration-200 transform rounded-sm group hover:scale-105"
+  >
+    <AnimatedPlaceholder class="max-w-[100%] h-full w-full" />
+  </div>
+</template>
