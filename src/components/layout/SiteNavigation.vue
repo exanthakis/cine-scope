b567dcafb0345b428820e0f782a24f837b899d0b
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-02-06 11:24:37 +0200

    Added hover effect to navigation & change font family

--- src/components/layout/SiteNavigation.vue
+++ src/components/layout/SiteNavigation.vue
@@ -6,17 +6,19 @@ import TheLogo from '../ui/TheLogo.vue'
 <template>
   <header class="flex justify-center w-full !px-[5vw] shadow-lg bg-film-tertiary">
     <nav
-      class="container flex flex-col items-center justify-between w-full gap-4 py-4 text-gray-400 sm:flex-row"
+      class="container flex flex-col items-center justify-between w-full gap-4 py-4 font-light text-gray-400 sm:flex-row"
     >
       <TheLogo />
       <ul class="flex items-center gap-3">
-        <li>
+        <li class="hover:text-film-primary">
           <RouterLink :to="{ name: 'movie-list' }">Search</RouterLink>
         </li>
-        <li>
+        <li class="hover:text-film-primary">
           <RouterLink :to="{ name: 'popular-list' }">Popular</RouterLink>
         </li>
-        <li><RouterLink :to="{ name: 'favorites' }">Favorites</RouterLink></li>
+        <li class="hover:text-film-primary">
+          <RouterLink :to="{ name: 'favorites' }">Favorites</RouterLink>
+        </li>
       </ul>
     </nav>
   </header>
