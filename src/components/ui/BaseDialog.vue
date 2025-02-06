469e1cddf554b90c237298d6b7650199fa99faec
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-02-06 16:43:19 +0200

    Create base dialog component and register globally

---
+++ src/components/ui/BaseDialog.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface BaseDialogProps {
+  show: boolean
+  title: string
+}
+defineProps<BaseDialogProps>()
+defineEmits(['close'])
+</script>
+
+<template>
+  <teleport to="body">
+    <div
+      v-if="show"
+      @click="$emit('close')"
+      class="fixed top-0 left-0 h-screen w-full bg-film-tertiary/75 z-10"
+    ></div>
+    <transition name="dialog">
+      <dialog
+        open
+        v-if="show"
+        class="fixed top-[20vh] left-0 r-0 w-[90%] rounded-xs ml-auto mr-auto max-w-xl z-100 border-0 shadow-xl p-4 m-0 overflow-hidden bg-film-secondary"
+      >
+        <header class="">
+          <slot name="header">
+            <h2 class="text-2xl">{{ title }}</h2>
+          </slot>
+        </header>
+        <section class="py-6">
+          <slot></slot>
+        </section>
+        <menu>
+          <slot name="actions"></slot>
+        </menu>
+      </dialog>
+    </transition>
+  </teleport>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import BaseButton from './components/ui/BaseButton.vue'
+import BaseDialog from './components/ui/BaseDialog.vue'
 
 const app = createApp(App)
 
-app.component('BaseButton', BaseButton)
+app.component('BaseButton', BaseButton).component('BaseDialog', BaseDialog)
 app.use(createPinia())
 app.use(router)
 
