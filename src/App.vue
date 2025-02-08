5804889161f02fb0c0b75a94d1c296d7b6417419
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-02-08 22:06:53 +0200

    Change main text color

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import SiteNavigation from './components/layout/SiteNavigation.vue'
 </script>
 
 <template>
-  <div class="relative flex flex-col items-center max-7xl min-h-[100vh] bg-radial-white">
+  <div
+    class="relative flex flex-col items-center max-7xl min-h-[100vh] bg-radial-white text-film-tertiary"
+  >
     <SiteNavigation />
 
     <RouterView />
