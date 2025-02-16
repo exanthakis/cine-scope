191edee00e1ac568461320542a78038724dba861
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-02-16 20:11:14 +0200

    Logo | Add tv icon

--- src/components/ui/TheLogo.vue
+++ src/components/ui/TheLogo.vue
@@ -2,6 +2,9 @@
 
 <template>
   <RouterLink :to="{ name: 'movie-list' }">
-    <span class="text-film-secondary text-2xl font-bold">CineQuest</span>
+    <div class="flex items-center gap-2">
+      <img alt="search-icon" class="" src="@/assets/icons/tv.svg" width="20" height="20" />
+      <span class="text-film-secondary text-2xl font-bold">CineQuest</span>
+    </div>
   </RouterLink>
 </template>
