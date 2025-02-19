4e6c5b31706922b3aa46337e09578258df7b42c8
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-02-19 16:42:11 +0200

    Site navigation | Added hover blur animation

--- src/components/layout/SiteNavigation.vue
+++ src/components/layout/SiteNavigation.vue
@@ -9,14 +9,14 @@ import TheLogo from '../ui/TheLogo.vue'
       class="flex w-full flex-col items-center justify-between gap-4 py-4 font-light text-gray-400 sm:flex-row"
     >
       <TheLogo />
-      <ul class="flex items-center gap-3">
-        <li class="hover:text-film-primary">
+      <ul class="nav-wrap flex items-center gap-3">
+        <li class="nav transition">
           <RouterLink :to="{ name: 'movie-list' }">Search</RouterLink>
         </li>
-        <li class="hover:text-film-primary">
+        <li class="nav transition">
           <RouterLink :to="{ name: 'popular-list' }">Popular</RouterLink>
         </li>
-        <li class="hover:text-film-primary">
+        <li class="nav transition">
           <RouterLink :to="{ name: 'favorites' }">Favorites</RouterLink>
         </li>
       </ul>
@@ -29,5 +29,17 @@ import TheLogo from '../ui/TheLogo.vue'
 nav .active {
   font-weight: bolder;
   color: #fff;
+}
+
+.nav-wrap:hover .nav {
+  filter: blur(1px);
+  opacity: 0.5;
+  box-shadow: none;
+}
+.nav-wrap:hover .nav:hover {
+  transform: scale(1.1);
+  filter: blur(0px);
+  opacity: 1;
+  box-shadow: 0 8px 20px 0px rgba(0, 0, 0, 0.125);
 }
 </style>
