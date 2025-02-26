95aa41e7ff04fe07453b5a98be2652c1e342070c
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-02-26 13:54:42 +0200

    Create avatar text component storybook

---
+++ src/stories/AvatarText.stories.ts
@@ -0,0 +1,45 @@
+import AvatarText from '@/components/ui/AvatarText.vue'
+import type { AvatarTextProps } from '@/types/general'
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+const meta = {
+  title: 'BaseComponents/AvatarText',
+  component: AvatarText,
+  tags: ['autodocs'],
+  argTypes: {
+    id: {
+      control: 'number',
+    },
+    name: {
+      control: 'text',
+    },
+    title: {
+      control: 'text',
+    },
+    path: {
+      control: 'text',
+    },
+  },
+  parameters: {
+    layout: 'centered',
+  },
+  render: (args: AvatarTextProps) => ({
+    components: { AvatarText },
+    setup() {
+      return { args }
+    },
+    template: '<AvatarText v-bind="args"/>',
+  }),
+} satisfies Meta<typeof AvatarText>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    id: 1,
+    name: 'Daniel Radcliffe',
+    title: 'Harry Potter',
+    path: '/iPg0J9UzAlPj1fLEJNllpW9IhGe.jpg',
+  },
+}
