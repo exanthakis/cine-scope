7ed2ccfab3f47be3e5d891885718433aca1c65f1
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-02-27 16:22:28 +0200

    Not found page | Edit text style

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -8,8 +8,16 @@ const { resource = 'page' } = defineProps<NotFoundProps>()
 
 <template>
   <div class="text-film-tertiary dark:text-film-secondary p-6 text-center">
-    <h1>Oops!</h1>
-    <h3>The {{ resource }} you're looking for is not here.</h3>
-    <RouterLink class="font-bold" :to="{ name: 'movie-list' }">Back to the home page</RouterLink>
+    <h1
+      class="text-primary-600 dark:text-primary-500 mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl"
+    >
+      404
+    </h1>
+    <p class="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl dark:text-white">
+      The {{ resource }} you're looking for is not here.
+    </p>
+    <RouterLink class="text-xl font-bold" :to="{ name: 'movie-list' }">
+      Back to the home page
+    </RouterLink>
   </div>
 </template>
