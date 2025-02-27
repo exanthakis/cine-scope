8cb026e84ebc6bd03b98e40dbbba5e02cf679ef2
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-02-27 16:12:54 +0200

    Network & notfound page | Fix text color for dark/light mode

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -7,7 +7,7 @@ const { resource = 'page' } = defineProps<NotFoundProps>()
 </script>
 
 <template>
-  <div class="p-6 text-center">
+  <div class="text-film-tertiary dark:text-film-secondary p-6 text-center">
     <h1>Oops!</h1>
     <h3>The {{ resource }} you're looking for is not here.</h3>
     <RouterLink class="font-bold" :to="{ name: 'movie-list' }">Back to the home page</RouterLink>
