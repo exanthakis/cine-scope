690e4c1baa3ae7694b18886b44fd005ccff6b028
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-03-11 13:22:18 +0200

    Base Badge | Create unit testing file

---
+++ src/components/__tests__/BaseBadge.test.ts
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import BaseBadge from '../ui/BaseBadge.vue'
+
+describe('BaseBadge', () => {
+  it('should render the title of the badge correctly', () => {
+    const title = 'Test title'
+
+    const wrapper = mount(BaseBadge, {
+      props: {
+        title,
+      },
+    })
+
+    expect(wrapper.text()).toContain(title)
+  })
+
+  it('should emits close event when button is clicked', async () => {
+    const title = 'Test title'
+
+    const wrapper = mount(BaseBadge, {
+      props: {
+        title,
+      },
+    })
+
+    await wrapper.find('button').trigger('click')
+    expect(wrapper.emitted('close')).toBeTruthy()
+  })
+})
