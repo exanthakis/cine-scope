1f3a9600a685650435ce1de89f4179be006ea42b
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-03-14 13:05:41 +0200

    Movie card skeleton | Change min height

--- src/components/movie/MovieCardSkeleton.vue
+++ src/components/movie/MovieCardSkeleton.vue
@@ -3,7 +3,7 @@ import AnimatedPlaceholder from '../ui/AnimatedPlaceholder.vue'
 </script>
 <template>
   <div
-    class="group relative mx-auto h-full min-h-[395px] w-full transform overflow-hidden rounded-sm transition duration-200 hover:scale-105 sm:min-h-[35vh]"
+    class="group relative mx-auto h-full min-h-[35vh] w-full transform overflow-hidden rounded-sm transition duration-200 hover:scale-105"
   >
     <AnimatedPlaceholder class="h-full w-full max-w-[100%]" />
   </div>
