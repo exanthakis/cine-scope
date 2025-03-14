6d6bba58a67de546917144682d3f880471ed2e36
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-03-14 13:38:21 +0200

    Router | Change history mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import HomeView from '@/views/HomeView.vue'
 import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
