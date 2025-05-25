0a3820e87490b715e4e88622648731b9b4f7fedb
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-05-25 20:17:18 +0300

    Movie cards | Added animation on limit change

--- src/components/movie/MovieCards.vue
+++ src/components/movie/MovieCards.vue
@@ -11,37 +11,57 @@ const moviesViewStore = useMoviesViewStore()
 </script>
 
 <template>
-  <div
-    class="max-2xl mx-auto grid grid-cols-2 gap-9 pb-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
-    v-show="moviesViewStore.view === MOVIES_VIEW.CARDS_VIEW"
-  >
-    <MovieCard
-      v-for="(movie, idx) in movies.slice(0, limit || movies.length)"
-      :key="movie.id"
-      :id="movie.id"
-      :title="movie.title"
-      :imgUrl="'poster_path' in movie ? movie.poster_path : movie.imgUrl"
-      :config="{
-        ...(showNum && { num: idx + 1 }),
-      }"
-    />
+  <div class="max-2xl mx-auto" v-show="moviesViewStore.view === MOVIES_VIEW.CARDS_VIEW">
+    <TransitionGroup
+      name="movie-fade"
+      tag="a"
+      class="grid grid-cols-2 gap-9 pb-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
+    >
+      <MovieCard
+        v-for="(movie, idx) in movies.slice(0, limit || movies.length)"
+        :key="movie.id"
+        :id="movie.id"
+        :title="movie.title"
+        :imgUrl="'poster_path' in movie ? movie.poster_path : movie.imgUrl"
+        :config="{
+          ...(showNum && { num: idx + 1 }),
+        }"
+      />
+    </TransitionGroup>
   </div>
-  <div
-    class="max-2xl mx-auto flex flex-col gap-9 pb-7"
-    v-show="moviesViewStore.view !== MOVIES_VIEW.CARDS_VIEW"
-  >
-    <ListCard
-      v-for="(movie, idx) in movies.slice(0, limit || movies.length)"
-      :key="movie.id"
-      :id="movie.id"
-      :title="movie.title"
-      :description="
-        'overview' in movie ? movie.overview : 'description' in movie ? movie.description : ''
-      "
-      :imgUrl="'poster_path' in movie ? movie.poster_path : movie.imgUrl"
-      :config="{
-        ...(showNum && { num: idx + 1 }),
-      }"
-    />
+  <div class="max-2xl mx-auto" v-show="moviesViewStore.view !== MOVIES_VIEW.CARDS_VIEW">
+    <TransitionGroup name="movie-fade" tag="a" class="flex flex-col gap-9 pb-7">
+      <ListCard
+        v-for="(movie, idx) in movies.slice(0, limit || movies.length)"
+        :key="movie.id"
+        :id="movie.id"
+        :title="movie.title"
+        :description="
+          'overview' in movie ? movie.overview : 'description' in movie ? movie.description : ''
+        "
+        :imgUrl="'poster_path' in movie ? movie.poster_path : movie.imgUrl"
+        :config="{
+          ...(showNum && { num: idx + 1 }),
+        }"
+      />
+    </TransitionGroup>
   </div>
 </template>
+
+<style scoped>
+.movie-fade-enter-from {
+  opacity: 0;
+  transform: scale(0.9);
+}
+.movie-fade-enter-to {
+  opacity: 1;
+  transform: scale(1);
+}
+.movie-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.movie-fade-leave-active {
+  transition: opacity 0.2s ease;
+  opacity: 0;
+}
+</style>
